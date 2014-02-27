a9383d1fb6851fc9c5a4db922cb122522a05f5e9
erikaji <erikaji@stanford.edu>
2014-02-27 01:41:37 +0000

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var projects = require('../projects.json');
+var grid = false;
 
 /*
  * GET home page.
  */
 
 exports.view = function(req, res){
+		projects.grid = false;
   	res.render('index', projects);
 };
+
+exports.viewGrid = function(req, res){
+		projects.grid = true;
+		res.render('index', projects);
+}
